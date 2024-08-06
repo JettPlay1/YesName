1ad6683065da10239bfc2f621ba2741789c5c843
JettPlay1 <jettplayyt@gmail.com>
2024-08-06 14:06:18 +0300

    Added shop

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -6,7 +6,8 @@ main_menu_kb = InlineKeyboardMarkup(
             inline_keyboard=[
                 [InlineKeyboardButton(text="Получить задание", callback_data="get_task")],
                 [InlineKeyboardButton(text="Получить слушателя", callback_data="get_listener")],
-                [InlineKeyboardButton(text="Оставить отзыв", callback_data="leave_feedback")]
+                [InlineKeyboardButton(text="Оставить отзыв", callback_data="leave_feedback")],
+                [InlineKeyboardButton(text="Магазин", callback_data="shop")]
             ]
         )
 
--- bot/routes.py
+++ bot/routes.py
@@ -8,18 +8,20 @@ from aiogram.filters import Command
 from bot.keyboards import *
 from bot import dp
 
+
 @dp.message(Command("start"))
 async def start_command(message: Message):
     await message.answer("Привет", reply_markup=main_menu_kb)
+
 
 @dp.callback_query(F.data == "main_menu")
 async def main_menu(callback: CallbackQuery):
     await callback.message.edit_text(text="Главное меню", reply_markup=main_menu_kb)
 
+
 @dp.callback_query(F.data == "get_task")
 async def get_task_command(callback: CallbackQuery):
     await callback.message.edit_text(text="Нажали Получить задание", reply_markup=return_back_kb)
-
 
 
 @dp.callback_query(F.data == "get_listener")
@@ -30,3 +32,8 @@ async def get_listener_command(callback: CallbackQuery):
 @dp.callback_query(F.data == "leave_feedback")
 async def leave_feedback_command(callback: CallbackQuery):
     await callback.message.edit_text(text="Нажали Оставить фидбек", reply_markup=return_back_kb)
+
+
+@dp.callback_query(F.data == "shop")
+async def main_menu(callback: CallbackQuery):
+    await callback.message.edit_text(text="МАГАЗИН", reply_markup=return_back_kb)
